5254a3b19f82aa011761d3f84c0bfe1b5d333646
mehmetcelmeli <firtinacelmeli@gmail.com>
2023-10-14 09:01:14 +0300

    updated

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
+import { CustomButtonProps } from "@/types";
 "use client";
 
-const CustomButton = () => {
+const CustomButton = ({title, containerStyles, handleClick}:CustomButtonProps) => {
   return (
     <button 
     disabled={false}
     type={"button"}
-    className={`custom-btn`}
-    onClick={() => {}}
-    ></button>
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}    
+    >
+      <span className={`flex-1`} >
+    {title}
+      </span>
+    </button>
   )
 }
 
