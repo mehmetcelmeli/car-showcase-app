33313473e71e73135d4d6ae147b67e6e3d3180c1
mehmetcelmeli <firtinacelmeli@gmail.com>
2023-10-16 11:16:10 +0300

    updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,17 @@ const Footer = () => {
             </div>
           ))}
         </div>
-        <div className=""></div>
+        <div className="flex justify-between items-center mt-10 border-t border-gray-100 sm:px-16 px-6 py-10 ">
+          <p>@2023 CarHub. All Rights Reserved</p>
+          <div className="footer__copyrights-link">
+            <Link href="/" className=" text-gray-500" >
+              Privacy Policy
+            </Link>
+            <Link href="/" className=" text-gray-500" >
+              Terms of Use
+            </Link>
+          </div>
+        </div>
       </div>
     </footer>
   );
