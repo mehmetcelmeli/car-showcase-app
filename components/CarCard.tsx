4de7c93c45a14a65a056856d5c787bf6693e0db5
mehmetcelmeli <firtinacelmeli@gmail.com>
2023-10-20 12:28:07 +0300

    feat: carcard is being constructed

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,16 +1,35 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Image from "next/image";
 
 import { CarProps } from "@/types";
-import CustomButton from './CustomButton';
+import CustomButton from "./CustomButton";
+import { calculateCarRent } from "@/utils";
 interface CarCardProps {
-    car:CarProps,
+  car: CarProps;
 }
 
-const CarCard = ({car}:CarCardProps) => {
-  const {city_mpg, make, model, transmission, year} =car;
-  return <div>CarCard</div>;
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, make, model, transmission, year } = car;
+  const carRent = calculateCarRent(city_mpg, year);
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+      <p className="flex mt-8 text-[32px] font-extrabold" >
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium">/day</span>
+      </p>
+      <div className="relative w-full h-40 my-3 object-contain">
+        <Image src="/hero.png"  alt="car model" fill priority className="object-contain"/>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default CarCard;
