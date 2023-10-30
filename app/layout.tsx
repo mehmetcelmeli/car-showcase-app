1a6030c923cfb8626fa25e2dc98ccb9536ce9831
mehmetcelmeli <firtinacelmeli@gmail.com>
2023-10-30 11:08:40 +0300

    feat: last controls

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import {Footer} from "@/components";
 
 export const metadata: Metadata = {
   title: "Car Hub",
-  description: "Discover the best cars in the world",
+  description: "Discover world's best car showcase application",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -17,18 +17,22 @@ async function Home({searchParams}:HomeProps) {
   return (
     <main className=" overflow-hidden">
       <Hero />
+
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold ">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
+
         <div className="home__filters">
           <SearchBar />
+
           <div className="home__filter-container">
             <CustomFilter title="fuel" options={fuels} />
             <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
+        
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
